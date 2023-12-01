9fb007e1e63443e2baf6cd830958e6b9714b6d50
umassdgithub <amir.umassd@gmail.com>
2023-12-01 08:52:44 -0500

    flask app

---
+++ D3js_Flask/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request,Response, render_template
+import requests
+import json
+import glob
+
+import pydicom
+app = Flask(__name__)
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+
+@app.route('/data', methods=['GET'])
+def returnData():
+    
+    fileNumber=request.args.get("file")
+    # Sanity Check
+    print(fileNumber)
+    if fileNumber is None:
+        fileNumber = "brain_001.dcm" 
+
+    try:
+        dicom_dataset = pydicom.dcmread(f"./Data/volume/{fileNumber}")
+        
+        return json.dumps(dicom_dataset.pixel_array.tolist())
+    except Exception as ex:
+        return str({"error":ex.__str__()})
+@app.route("/files")
+def files():
+    files_list = [fl.split("/")[-1] for fl in glob.glob("./Data/volume/*.dcm")]
+    return json.dumps(files_list)
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5001, debug=True)
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Flask_D3JS
- 
+# Using APIs with D3js
+
+In this example a flask based Python application acts as the server for the data visualization tool.
+
+<img src="img/preview.png">
